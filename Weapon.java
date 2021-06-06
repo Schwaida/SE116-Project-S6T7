209bb15cc608598d856fe4ec1acb2d1af06739bf
Schwaida <60405166+Schwaida@users.noreply.github.com>
2021-06-06 16:38:08 +0300

    Add files via upload

---
+++ Weapon.java
@@ -0,0 +1,47 @@
+public class Weapon {
+    private int weaponDamage;
+    private int weaponBlockPower;
+    private int weaponRange;
+    private int weaponValue;
+    private int weaponWeight;
+
+    public void setWeaponDamage(int damage){
+        weaponDamage = damage;
+    }
+
+    public int getWeaponDamage(){
+        return weaponDamage;
+    }
+
+    public void setWeaponBlockPower(int blockPower){
+        weaponBlockPower = blockPower;
+    }
+
+    public int getWeaponBlockPower(){
+        return weaponBlockPower;
+    }
+
+    public void setWeaponRange(int range){
+        weaponRange = range;
+    }
+
+    public int getWeaponRange(){
+        return weaponRange;
+    }
+
+    public void setWeaponValue(int value){
+        weaponValue = value;
+    }
+
+    public int getWeaponValue(){
+        return weaponValue;
+    }
+
+    public void setWeaponWeight(int weight){
+        weaponWeight = weight;
+    }
+
+    public int getWeaponWeight(){
+        return weaponWeight;
+    }
+}
